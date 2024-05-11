339b3a996783af11b965f2028e8ca04781bdcc63
Chitransh Saxena <chitransh033@gmail.com>
2024-05-11 14:57:42 +0530

    Proof of me not writing a bulletproof code

--- src/test/java/com/me/DSA/graph/algorithms/TopoSortTest.java
+++ src/test/java/com/me/DSA/graph/algorithms/TopoSortTest.java
@@ -29,6 +29,31 @@ public class TopoSortTest {
 
 
         List<Integer> sorted = topo.sort(graph);
+        Assertions.assertEquals(graph.size(), sorted.size());
+        System.out.println(sorted);
+    }
+
+    @Test
+    public void testTopoSortWithCycle() {
+
+        // Since my code keeps the visited track, cycles are handled.
+        // But in case there is a cycle, this code still produces results. Inconsistent results.
+
+        TopoSort topo = new TopoSort();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        graph.put(5, List.of(6,7));
+        graph.put(6, List.of(5));
+        graph.put(1, List.of(3));
+        graph.put(2, Arrays.asList(3, 4));
+        graph.put(3, List.of(5));
+        graph.put(4, List.of(7));
+        graph.put(7, List.of(8));
+        graph.put(8, List.of());
+
+
+        List<Integer> sorted = topo.sort(graph);
+        Assertions.assertEquals(graph.size(), sorted.size());
         System.out.println(sorted);
     }
 }
