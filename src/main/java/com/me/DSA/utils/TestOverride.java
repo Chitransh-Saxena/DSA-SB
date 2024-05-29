b40f498ea542333466f426217ba48f90e5f65bd2
Chitransh Saxena <chitransh033@gmail.com>
2024-05-29 19:57:44 +0530

    Java - Compilation error in different return type same parameter method names

---
+++ src/main/java/com/me/DSA/utils/TestOverride.java
@@ -0,0 +1,13 @@
+package com.me.DSA.utils;
+
+public class TestOverride {
+
+    public void clear() {
+        System.out.println("This clears nothing");
+    }
+
+    // Uncomment below code to see compilation error
+//    public int clear() {
+//        return 1;
+//    }
+}
